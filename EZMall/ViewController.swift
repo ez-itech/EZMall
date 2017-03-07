19a5d528bf03febb36c238d7d7cc4c1d06436d6e
wangan <WA@wangandemac.local>
2017-03-07 22:07:46 +0800

    Initial Commit

---
+++ EZMall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EZMall
+//
+//  Created by wangan on 17/3/6.
+//  Copyright © 2017年 wangan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
